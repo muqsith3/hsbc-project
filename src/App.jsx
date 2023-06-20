be9150d07b7fd1fb6c6b2a1545a99551eea597b4
muqsith3 <alimuqsith03@icloud.com>
2023-06-20 23:13:54 +0700

    first commit, setup react by vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-full bg-green-500">
+        <div className="container bg-slate-500 m-auto">
+          <h1 className="text-2xl text-red-500 ">
+            project grop 3- untuk final project
+          </h1>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
